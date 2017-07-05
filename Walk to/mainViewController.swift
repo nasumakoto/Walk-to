dc8e85f0527d7e3e4ecea2c8fc7b446c93609f2e
那須真 <flamingo.nasu@gmail.com>
2017-07-05 15:18:21 +0800

    授業中の問題箇所でコミットプッシュ

--- Walk to/mainViewController.swift
+++ Walk to/mainViewController.swift
@@ -6,9 +6,9 @@
 //  Copyright © 2017年 Makoto Nasu. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import HealthKit
-import CoreMotion
 
 class mainViewController: UIViewController, UITextFieldDelegate {
 
@@ -23,6 +23,8 @@ class mainViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var nextDistance: UILabel!
     
     var timer = Timer()
+    
+    let healthKitStore = HKHealthStore()
     
  
     
@@ -51,7 +53,23 @@ class mainViewController: UIViewController, UITextFieldDelegate {
         
         timer = Timer.scheduledTimer(timeInterval: 0.00, target: self, selector: #selector(mainViewController.go), userInfo: nil, repeats: true)
         
-
+        if(checkAuthorization())
+        {
+            if(HKHealthStore.isHealthDataAvailable())
+            {
+                recentSteps() { steps, error in
+                    DispatchQueue.main.async {
+//                        self.myTotalDistance.text = String(format:"%.0f", steps)
+                    }
+                }
+                
+                
+                
+            }
+        }
+        
+        
+        
     }
 
     
@@ -63,6 +81,89 @@ class mainViewController: UIViewController, UITextFieldDelegate {
         totalProgress.transform = CGAffineTransform(scaleX: 1.0, y: 7.0)
     
     }
+    
+    func updateStepCount()
+    {
+        
+    }
+    
+    func checkAuthorization() -> Bool
+    {
+        var isEnabled = true
+        
+        if HKHealthStore.isHealthDataAvailable()
+        {
+            let healthKitTypesToRead : Set = [
+                HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.dateOfBirth)!,
+                HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.bloodType)!,
+                HKObjectType.characteristicType(forIdentifier: HKCharacteristicTypeIdentifier.biologicalSex)!,
+                HKObjectType.quantityType(forIdentifier:HKQuantityTypeIdentifier.stepCount)!,
+                HKObjectType.workoutType(),
+                HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!
+            ]
+            
+            healthKitStore.requestAuthorization(toShare: nil, read: healthKitTypesToRead, completion: { (success, error) in
+                isEnabled = success
+            })
+        }
+        else
+        {
+            isEnabled = false
+        }
+        
+        return isEnabled
+    }
+    
+    func recentSteps(completion: @escaping (Double, NSError?) -> () )
+    {
+        let healthKitTypesToRead = HKSampleType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)
+        
+        
+        let calendar = Calendar.current
+        let yesterday = calendar.date(byAdding: Calendar.Component.hour, value: -24, to: Date())
+        
+        
+        let predicate = HKQuery.predicateForSamples(withStart: yesterday, end: Date(), options: [])
+        
+        
+        
+        let query = HKSampleQuery(sampleType: healthKitTypesToRead!, predicate: predicate, limit: 0, sortDescriptors: nil) { query, results, error in
+            var steps: Double = 0
+            var distance: Double = 0
+            var distanceInt:Int = 0
+            
+            if results != nil {
+                if (results?.count)! > 0
+                {
+                    for result in results as! [HKQuantitySample]
+                    {
+                        if(result.device?.model == "iPhone")
+                        {
+                            
+                            //                            steps += result.quantity.doubleValue(for: HKUnit.count())
+                            //何メートル換算で距離を取得
+                            distance += result.quantity.doubleValue(for: HKUnit.meter())
+                            print(result.quantity.doubleValue(for: HKUnit.meter()))
+                            distanceInt = Int(distance)
+                            self.myTotalDistance.text = "\(distanceInt)"
+                        }
+                    }
+                }
+                
+                
+            }
+            
+            if error != nil {
+                completion(distance, error as! NSError)
+            }
+            
+        }
+        
+        healthKitStore.execute(query)
+    }
+
+  
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
